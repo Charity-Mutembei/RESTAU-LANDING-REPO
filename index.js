51399aa5b0494d7049e4e6986e7781ff27387249
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-03-21 15:54:01 +0300

    Changed the size display

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 //delete functioh
+let cart = getElementById('cart');
+if (document.readyState =="loading"){
+    document.addEventListener("DOMcontentloaded", ready);
+}else{
+    ready();
+}
 function ready(){
-    
+    var removeItemsFromCart = document.getElementById('trash')
+    console.log(removeItemsFromCart)
+    for (var i = 0; i < removeItemsFromCart.length; i++){
+        var but = removeItemsFromCart[i]
+        but.addEventListener('click', removecartitems)
+    }
+}
+function removeItemsFromCart(event){
+    var butClicked = event.target
+    butClicked.parentElement.remove()
 }
